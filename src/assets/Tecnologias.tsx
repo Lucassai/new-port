a80ce65f175bf9f51e13c08cadba3291063f68cd
Salada <lucasvmo42@gmail.com>
2025-08-19 16:42:23 -0300

    update:botoes ta tela de tecnologias

--- src/assets/Tecnologias.tsx
+++ src/assets/Tecnologias.tsx
@@ -16,131 +16,124 @@ function Tecnologias() {
         <h1 className="text-2xl font-bold text-cyan-50 text-center">
           Tecnologias
         </h1>
-        <ul className="flex flex-col space-y-4 mt-4">
+        <ul className="grid grid-cols-2 gap-4 mt-4">
           <a
-            className="text-amber-50 hover:underline"
             href="https://vitejs.dev/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <li className="flex items-center">
-              <SiVite className="text-amber-50 mr-2" />
-              <span className="text-amber-50">Vite</span>
+            <li className="botao_tecnologias">
+              <SiVite />
+              <span>Vite</span>
             </li>
           </a>
+
           <a
-            className="text-amber-50 hover:underline"
             href="https://react.dev/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <li className="flex items-center">
-              <FaReact className="text-amber-50 mr-2" />
-              <span className="text-amber-50">React</span>
+            <li className="botao_tecnologias">
+              <FaReact />
+              <span>React</span>
             </li>
           </a>
+
           <a
-            className="text-amber-50 hover:underline"
             href="https://www.java.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <li className="flex items-center">
-              <FaJava className="text-amber-50 mr-2" />
-              <span className="text-amber-50">Java</span>
+            <li className="botao_tecnologias">
+              <FaJava />
+              <span>Java</span>
             </li>
           </a>
+
           <a
-            className="text-amber-50 hover:underline"
             href="https://www.w3.org/TR/html52/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <li className="flex items-center">
-              <FaHtml5 className="text-amber-50 mr-2" />
-              <span className="text-amber-50">HTML5</span>
+            <li className="botao_tecnologias">
+              <FaHtml5 />
+              <span>HTML5</span>
             </li>
           </a>
+
           <a
-            className="text-amber-50 hover:underline"
             href="https://www.w3.org/Style/CSS/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <li className="flex items-center">
-              <FaCss3 className="text-amber-50 mr-2" />
-              <span className="text-amber-50">CSS3</span>
+            <li className="botao_tecnologias">
+              <FaCss3 />
+              <span>CSS3</span>
             </li>
           </a>
 
           <a
-            className="text-amber-50 hover:underline"
             href="https://www.javascript.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <li className="flex items-center">
-              <FaJs className="text-amber-50 mr-2" />
-              <span className="text-amber-50">JavaScript</span>
+            <li className="botao_tecnologias">
+              <FaJs />
+              <span>JavaScript</span>
             </li>
           </a>
 
           <a
-            className="text-amber-50 hover:underline"
             href="https://tailwindcss.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <li className="flex items-center">
-              <RiTailwindCssFill className="text-amber-50 mr-2" />
-              <span className="text-amber-50">Tailwind CSS</span>
+            <li className="botao_tecnologias">
+              <RiTailwindCssFill />
+              <span>Tailwind CSS</span>
             </li>
           </a>
 
           <a
-            className="text-amber-50 hover:underline"
             href="https://nestjs.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <li className="flex items-center">
-              <SiNestjs className="text-amber-50 mr-2" />
-              <span className="text-amber-50">NestJS</span>
+            <li className="botao_tecnologias">
+              <SiNestjs />
+              <span>NestJS</span>
             </li>
           </a>
 
           <a
-            className="text-amber-50 hover:underline"
             href="https://www.prisma.io/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <li className="flex items-center">
-              <SiPrisma className="text-amber-50 mr-2" />
-              <span className="text-amber-50">Prisma</span>
+            <li className="botao_tecnologias">
+              <SiPrisma />
+              <span>Prisma</span>
             </li>
           </a>
           <a
-            className="text-amber-50 hover:underline"
             href="https://www.mysql.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <li className="flex items-center">
-              <SiMysql className="text-amber-50 mr-2" />
-              <span className="text-amber-50">MySQL</span>
+            <li className="botao_tecnologias">
+              <SiMysql />
+              <span>MySQL</span>
             </li>
           </a>
 
           <a
-            className="text-amber-50 hover:underline"
             href="https://www.docker.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <li className="flex items-center">
-              <FaDocker className="text-amber-50 mr-2" />
-              <span className="text-amber-50">Docker</span>
+            <li className="botao_tecnologias">
+              <FaDocker />
+              <span>Docker</span>
             </li>
           </a>
         </ul>
